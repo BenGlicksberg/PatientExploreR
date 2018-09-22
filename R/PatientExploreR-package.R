2707a74db356a0b703f439d4cc188763ee3c98e8
MABadge <mabadgeley@gmail.com>
2018-09-22 19:01:29 -0400

    update and document pkg ontology map

---
+++ R/PatientExploreR-package.R
@@ -0,0 +1,22 @@
+#' PatientExploreR
+#'
+#' Demonstrate PatientExploreR implementation
+#'
+#' @name PatientExploreR
+#' @docType package
+"_PACKAGE"
+
+
+#' Ontology concept associations
+#'
+#' 3,540,241 obs. of 6 variables
+#'
+#' \itemize{
+#'   \item *concept_id: integer identifier
+#'   \item *concept_name: string term
+#'   \item *...
+#' }
+#'
+#' @name dataOntology
+#' @docType data
+"dataOntology"
