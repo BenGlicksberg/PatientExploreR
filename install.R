bfc309801f646c9e9d07bfa2a44e1354bcce0a03
Ben Glicksberg <Ben.glicksberg@gmail.com>
2019-02-26 16:49:33 -0800

    Update install.R
    updated install.R to include github specific packages

--- install.R
+++ install.R
@@ -4,7 +4,7 @@
 ## 9/2018
 #####################################################
 
-install.packages(c("devtools","shiny","shinyWidgets","shinyjs","shinyalert","shinythemes","DBI","RMySQL","plotly","timevis","stringr","dplyr","data.table","purrr","DT"))
+install.packages(c("devtools","shiny","shinyWidgets","shinyjs","shinythemes","DBI","RMySQL","plotly","stringr","dplyr","data.table","purrr"))
 
 # install OHDSI packages
 library(devtools)
@@ -12,3 +12,9 @@ library(devtools)
 devtools::install_github("ohdsi/DatabaseConnectorJars")
 devtools::install_github("ohdsi/DatabaseConnector")
 devtools::install_github("ohdsi/SqlRender")
+
+# install specific versions of other packages
+devtools::install_github("rstudio/DT")
+devtools::install_github("daattali/timevis")
+devtools::install_github("daattali/shinyalert")
+
